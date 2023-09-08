5162261371dfa06c8b9619e62ca13cfff3d04ff9
Nodirbek <99579342+Nodirbek24081999@users.noreply.github.com>
2023-09-08 11:25:31 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* const yourAge=confirm("sizning yoshingiz 18 dan kattami" )
+if (yourAge==true) {
+    console.log("siz saytdan foydalana olasiz")
+} else {
+    console.log('siz saytdan foydalana olmaysiz')
+}
+*/
+/*let favouritColour=[]
+favouritColour[0]=prompt("what is your favourit colour #1?","")
+favouritColour[1]=prompt("what is your favourit colour #2?","")
+favouritColour[2]=prompt("what is your favourit colour #3?","")
+console.log(favouritColour)*/
+const number = prompt('yoshingiz nechada')
+let age  = (2023- number)
+console.log(age)
